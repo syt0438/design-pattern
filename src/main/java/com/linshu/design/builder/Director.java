84dfc24015cddb1014f0e8de14cc4b331f7758ef
songyutao <syt0438@163.com>
2022-12-27 12:23:37 +0800

    原型模式

---
+++ src/main/java/com/linshu/design/builder/Director.java
@@ -0,0 +1,10 @@
+package com.linshu.design.builder;/**
+ * 
+ *
+ * @author LinShu
+ * Just contact me if you have any questions.
+ * My e-mail is bruce.song@backgardon.com.
+ * Date: 2022/12/22 
+ * Time: 18:37
+ */public class Director {
+}
