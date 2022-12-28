214ea5301363ec88183433f363b48612e55ac873
songyutao <syt0438@163.com>
2022-12-28 17:12:19 +0800

    观察者模式

---
+++ src/main/java/com/linshu/design/observer2/NewsPaper.java
@@ -0,0 +1,25 @@
+package com.linshu.design.observer2;
+
+import lombok.Getter;
+
+import java.util.Observable;
+
+/**
+ * @author LinShu
+ * Just contact me if you have any questions.
+ * My e-mail is bruce.song@backgardon.com.
+ * Date: 2022/12/28
+ * Time: 16:59
+ */
+@Getter
+class NewPaper extends Observable {
+
+    private String data;
+
+    public void setData(String data) {
+        this.data = data;
+
+        setChanged();
+        notifyObservers(data);
+    }
+}
---
+++ src/main/java/com/linshu/design/observer2/Reader.java
@@ -0,0 +1,10 @@
+package com.linshu.design.observer2;/**
+ * 
+ *
+ * @author LinShu
+ * Just contact me if you have any questions.
+ * My e-mail is bruce.song@backgardon.com.
+ * Date: 2022/12/28 
+ * Time: 17:03
+ */public class Reader {
+}
